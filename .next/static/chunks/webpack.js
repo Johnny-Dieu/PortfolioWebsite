6c35599e03c62c3b5097067e86817df7874aed87
Johnny-Dieu <46733028+Johnny-Dieu@users.noreply.github.com>
2021-12-14 22:32:50 -0500

    svg removal

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d6034188e52d9234aa55"; }
+/******/ 		__webpack_require__.h = function() { return "240cd3e527c01dda2cf7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.77bed5106effb83ae5c0.hot-update.js
+++ .next/static/webpack/webpack.77bed5106effb83ae5c0.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "f4cc579437e50f80717d"; }
+/******/ 	__webpack_require__.h = function() { return "240cd3e527c01dda2cf7"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
